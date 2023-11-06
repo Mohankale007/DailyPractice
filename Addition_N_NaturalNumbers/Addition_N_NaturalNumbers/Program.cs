c5ef5df7d14549d7499c128b72b09d314ee9406f
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-06 15:26:41 +0530

    Add files via upload

---
+++ Addition_N_NaturalNumbers/Addition_N_NaturalNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Addition_N_NaturalNumbers
+{
+    internal class Program
+    {
+        public static void Addition(int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                count += i;
+            }
+            System.Console.WriteLine("Addition of N natural number:" + count);
+
+        }
+        static void Main(string[] args)
+        {
+            Program.Addition(3);
+            Console.ReadLine();
+        }
+    }
+}
