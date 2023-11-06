059a20969b14ad2b194ebb5f431ca2a82827b2b6
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-06 23:12:48 +0530

    Add files via upload

---
+++ CelsiusToFahrenheitConversion/CelsiusToFahrenheitConversion/Conversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusToFahrenheitConversion
+{
+    internal class Conversion
+    {
+        public double FahrenheitToCelsius(string fahrenheit)
+        {    //°C = (°F − 32) x 5/9
+            double fara=double.Parse(fahrenheit);
+            double result = (fara - 32) * 5 / 9;
+            return result;
+        }
+        public double CelsiusToFahrenheit(string Celsius)
+        {    //°F = (°C × 9/5) + 32
+            double cel = double.Parse(Celsius);
+            double result = (cel *9/5) +32;
+            return result;
+        }
+    }
+}
---
+++ CelsiusToFahrenheitConversion/CelsiusToFahrenheitConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CelsiusToFahrenheitConversion
+{
+
+// Multiply the °C temperature by 1.8. Add 32 to this number.This is the answer in °F.
+// °F = (°C × 9/5) + 32
+//It's just as easy to convert Fahrenheit to Celcius;
+//°C = (°F − 32) x 5/9
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conversion conversion = new Conversion();
+            Console.WriteLine("Enter Fahrenheit Value in F:");
+            string val1=Console.ReadLine();
+            double result1= conversion.FahrenheitToCelsius(val1);
+            Console.WriteLine(result1+" C");
+
+            Console.WriteLine("Enter Celsius Value in C");
+            string val2=Console.ReadLine();
+            double result2 = conversion.CelsiusToFahrenheit(val2);
+            Console.WriteLine(result2+" F");
+            Console.ReadLine();
+            
+        }
+    }
+}
