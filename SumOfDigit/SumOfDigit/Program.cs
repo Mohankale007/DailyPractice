dd2aeddadef92add26d9e3b96118fc40702256be
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-06 23:09:25 +0530

    Add files via upload

---
+++ SumOfDigit/SumOfDigit/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SumOfDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= 1234;//1+2+3+4=10
+            int reminder;
+            int sum = 0;
+
+            while (n > 0)
+            {
+            
+                reminder = n % 10;
+                sum = sum + reminder;
+                n = n / 10;
+            }
+            Console.WriteLine(sum);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
