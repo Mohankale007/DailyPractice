92b539a48c1cf650c1c60ca25ab8243cdad2ab74
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 22:58:52 +0530

    Add files via upload

---
+++ PalindromeNumberOrNot/PalindromeNumberOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PalindromeNumberOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            int num=int.Parse(Console.ReadLine());
+
+            int reminder, result = 0;
+            int con = num;
+            while(num>0)
+            {
+                reminder = num % 10;
+                result = result * 10 + reminder;
+                num=num / 10;
+            }
+
+            
+            if (result == con)
+            {
+                Console.WriteLine("Number is palindrome:" + result);
+            }
+            else
+            {
+                Console.WriteLine("number is not palindrome...!");
+            }
+        }
+    }
+}
