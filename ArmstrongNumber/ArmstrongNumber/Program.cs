79adac672a6f1dd437a6e0e067928319d3667eb7
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 22:58:12 +0530

    Add files via upload

---
+++ ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            int num=int.Parse(Console.ReadLine());
+            int remider, result = 0;
+            int sum = num;
+            while (num > 0)
+            {
+
+                remider = num%10;
+                result = result+(remider*remider*remider);
+                num = num/10;
+            }
+            if (result == sum)
+            {
+                Console.WriteLine("Number is Armstrong...!");
+            }
+            else
+            {
+
+                Console.WriteLine("Number is not Armstrong");
+            }
+        }
+    }
+}
