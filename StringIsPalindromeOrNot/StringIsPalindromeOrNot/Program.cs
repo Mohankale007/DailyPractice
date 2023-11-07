82ceebb5efbd4292325f76cb9ed46c336bd0d4e0
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 23:01:41 +0530

    Add files via upload

---
+++ StringIsPalindromeOrNot/StringIsPalindromeOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StringIsPalindromeOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            string s1=Console.ReadLine().ToLower();
+            string s2 = "";
+            char[] arr = s1.ToCharArray();
+
+            for(int i=arr.Length-1; i>=0; i--)
+            {
+                s2=s2 + arr[i];
+
+            }
+            if(s2 == s1)
+            {
+                Console.WriteLine("String is Palindrome..!");
+            }
+            else
+            {
+                Console.WriteLine("String is not Palidrome..!");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
