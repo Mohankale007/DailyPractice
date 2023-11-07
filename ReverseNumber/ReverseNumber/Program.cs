d472a3888b95e95ef16d34faef57bcca13f640bc
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 23:00:21 +0530

    Add files via upload

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 123;
+            int reminder,reverse=0;
+            while(n>0)
+            {
+                reminder = n % 10;
+                reverse = reverse * 10 + reminder;
+                n = n / 10;
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
