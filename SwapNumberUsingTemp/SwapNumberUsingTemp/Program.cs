0c85e52d7a7964957789147ad733feb7ebbd92f6
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 23:02:19 +0530

    Add files via upload

---
+++ SwapNumberUsingTemp/SwapNumberUsingTemp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SwapNumberUsingTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 20;
+
+            int temp;
+
+            //temp=num1;
+            //num1 = num2;
+            //num2=temp;
+
+            num1 =num1+ num2;
+            num2=num1-num2;
+            num1=num1-num2;
+
+
+            Console.WriteLine("swap numbes:"+num1+" "+num2);
+
+        }
+    }
+}
