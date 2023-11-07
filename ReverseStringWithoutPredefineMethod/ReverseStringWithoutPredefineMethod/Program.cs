8b4d1e1c035e82a119cec4cfb1f34618883480ee
Mohan Kale <119037071+Mohankale007@users.noreply.github.com>
2023-11-07 23:01:01 +0530

    Add files via upload

---
+++ ReverseStringWithoutPredefineMethod/ReverseStringWithoutPredefineMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseStringWithoutPredefineMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string s1=Console.ReadLine();
+
+            string s2 = "";
+
+            char[] arr = s1.ToCharArray();
+
+            for(int i = arr.Length-1; i >=0; i--)
+            {
+                s2=s2+ arr[i];
+            }
+            Console.WriteLine("Reversed String:"+s2);
+
+            Console.ReadLine();
+        }
+    }
+}
